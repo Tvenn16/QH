c87ce8b17e0ad3750a06d78444e1f14872a7a8c6
QuantumH <conrado.steven2003@gmail.com>
2026-06-09 11:09:00 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 local Games = {
     [107646426076756] = "https://raw.githubusercontent.com/Tvenn16/QH/main/BARFQH.lua",      -- Build a Ring Farm
-    [79268393072444]  = "https://raw.githubusercontent.com/Tvenn16/QH/main/LEMONSQH.lua",    -- Sell Lemons
+    [7395930870]  = "https://raw.githubusercontent.com/Tvenn16/QH/main/LEMONSQH.lua",    -- Sell Lemons
 }
 
 return Games

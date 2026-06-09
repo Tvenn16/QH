02e376cd90bff41a79619e635339ea9cb0c4f983
QuantumH <conrado.steven2003@gmail.com>
2026-06-09 11:14:41 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-    [107646426076756] = "https://raw.githubusercontent.com/Tvenn16/QH/main/BARFQH.lua",
+    [10039338037] = "https://raw.githubusercontent.com/Tvenn16/QH/main/BARFQH.lua",
     [7395930870] = "https://raw.githubusercontent.com/Tvenn16/QH/main/LEMONSQH.lua",
 }
 
